a2b3df05120c9a3c802ec2a51433a80982c542e9
Noorbala7418 <noorbala7418@gmail.com>
2024-08-25 03:28:50 +0330

    Add total traffic to get configs with prefix

--- pkg/tools/tools.go
+++ pkg/tools/tools.go
@@ -3,6 +3,7 @@ package tools
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
 const ONE_KB = 1024
@@ -11,20 +12,26 @@ const ONE_GB = ONE_MB * 1024
 const ONE_TB = ONE_GB * 1024
 const ONE_PB = ONE_TB * 1024
 
-func SizeFormat(size int) string {
+func SizeFormat(size int, prc ...int) string {
+	digitNumbers := 2
+	if len(prc) > 0 {
+		if prc[0] > 0 {
+			digitNumbers = prc[0]
+		}
+	}
 	size64 := float64(size)
 	if size < ONE_KB {
-		return fmt.Sprintf("%.2f", toFixed(size64, 0)) + " B"
+		return fmt.Sprintf("%."+strconv.Itoa(digitNumbers)+"f", toFixed(size64, 0)) + " B"
 	} else if size < ONE_MB {
-		return fmt.Sprintf("%.2f", toFixed((size64/ONE_KB), 2)) + " KB"
+		return fmt.Sprintf("%."+strconv.Itoa(digitNumbers)+"f", toFixed((size64/ONE_KB), 2)) + " KB"
 	} else if size < ONE_GB {
-		return fmt.Sprintf("%.2f", toFixed((size64/ONE_MB), 2)) + " MB"
+		return fmt.Sprintf("%."+strconv.Itoa(digitNumbers)+"f", toFixed((size64/ONE_MB), 2)) + " MB"
 	} else if size < ONE_TB {
-		return fmt.Sprintf("%.2f", toFixed((size64/ONE_GB), 2)) + " GB"
+		return fmt.Sprintf("%."+strconv.Itoa(digitNumbers)+"f", toFixed((size64/ONE_GB), 2)) + " GB"
 	} else if size < ONE_PB {
-		return fmt.Sprintf("%.2f", toFixed((size64/ONE_TB), 2)) + " TB"
+		return fmt.Sprintf("%."+strconv.Itoa(digitNumbers)+"f", toFixed((size64/ONE_TB), 2)) + " TB"
 	} else {
-		return fmt.Sprintf("%.2f", toFixed((size64/ONE_PB), 2)) + " PB"
+		return fmt.Sprintf("%."+strconv.Itoa(digitNumbers)+"f", toFixed((size64/ONE_PB), 2)) + " PB"
 	}
 }
 
